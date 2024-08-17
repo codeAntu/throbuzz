3b4143d513c5bb5697415e18477343af9790f27c
Ananta <codeantu@gmail.com>
2024-08-17 11:39:45 +0530

    refactor: Update Screen component props and styles
    Update the Screen component to use the ComponentProps type from React and spread the rest of the props onto the wrapping div. This allows for more flexibility when using the Screen component and ensures that any additional props are correctly passed down to the underlying div element. Also, refactor the className prop to be optional and provide a default empty string value. This improves the readability and maintainability of the code.

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React, { ComponentProps } from 'react'
 
-type ScreenProps = {
-  className?: string
-  children: React.ReactNode
-}
+type ScreenProps = ComponentProps<'div'>
 
-export function Screen({ className, children }: ScreenProps) {
+export function Screen({ children, className, ...rest }: ScreenProps) {
   return (
     <div className={`flex w-full items-center justify-center px-5`}>
-      <div className={`flex h-[100dvh] max-w-[500px] flex-col py-5 ${className}`}>{children}</div>
+      <div className={`flex h-[100dvh] max-w-[500px] flex-col py-5 ${className || ''}`} {...rest}>
+        {children}
+      </div>
     </div>
   )
 }
