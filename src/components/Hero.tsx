ee270b32531b7acc65beb36ff1680a990e9d0d96
Ananta <codeantu@gmail.com>
2024-08-17 22:25:03 +0530

    refactor: Update Hero component styles
    Update the Hero component styles to improve the visual appeal and consistency. This includes adjusting the gap between elements, updating the text color, and adding a welcome message. These changes aim to enhance the overall design of the Hero section in the Throbuzz application.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,12 @@
 export default function Hero() {
   return (
-    <div className='flex flex-col items-center justify-center gap-10 text-black dark:text-white'>
+    <div className='flex flex-col items-center justify-center gap-9 text-black dark:text-white/5'>
       <img src='./icons/icon.png' alt='' className='w-1/2 sm:w-2/5' />
       <div className='flex w-4/5 flex-col items-center justify-center gap-2'>
         <div className='text-3xl font-semibold text-cyan-500'>Throbuzz</div>
-        <div className='text-center text-xs font-semibold text-black/60 dark:text-white/50'>
-          Welcome to Throbuzz . Please log in or create an account to continue.
+        <div className='flex flex-col items-center justify-center text-center text-xs font-semibold text-black/40 dark:text-white/40'>
+          <div>Welcome to Throbuzz </div>
+          <div>Please log in or create an account to continue</div>
         </div>
       </div>
     </div>
