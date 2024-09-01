f8a3a49caf37a9bff085147f7121cb8ae2484abe
Ananta <codeantu@gmail.com>
2024-09-01 18:30:10 +0530

    refactor: Implement lazy loading for profile images and remove unused routes

--- src/app/(client)/(user)/profile/[userName]/edit/page.tsx
+++ src/app/(client)/(user)/profile/[userName]/edit/page.tsx
@@ -1,9 +1,166 @@
+'use client'
+import { Button } from '@/components/Button'
+import Error from '@/components/Error'
+import { Ic } from '@/components/Icon'
+import Input from '@/components/Input'
 import { Screen } from '@/components/Screen'
+import axios from 'axios'
+import { AtSign, Check, LoaderCircle, User, X } from 'lucide-react'
+import { useEffect, useState } from 'react'
 
 export default function Edit() {
+  const [user, setUser] = useState({
+    name: '',
+    bio: '',
+    about: '',
+  })
+
+  const [username, setUsername] = useState('')
+  const [error, setError] = useState('')
+  const [isUsernameAvailable, setIsUsernameAvailable] = useState(false)
+  const [isUsernameChecking, setIsUsernameChecking] = useState(false)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      checkUsername()
+    }, 300)
+    return () => clearTimeout(timeout)
+  }, [username])
+
+  useEffect(() => {
+    setError('')
+  }, [user, username])
+
+  async function checkUsername() {
+    if (username.length < 3) {
+      setIsUsernameAvailable(false)
+      return
+    }
+    setIsUsernameChecking(true)
+
+    try {
+      const response = await axios.post('/api/auth/check-username', { username: username })
+      if (await response.data.success) {
+        setIsUsernameAvailable(true)
+      } else {
+        setIsUsernameAvailable(false)
+      }
+      console.log('response', response.data.success)
+    } catch (error: any) {
+      console.log('error', error.response.data)
+    } finally {
+      setIsUsernameChecking(false)
+    }
+  }
+
+  async function updateUser() {
+    if (username.length < 3) {
+      setError('Username must be atleast 3 characters long')
+      return
+    }
+
+    if (!isUsernameAvailable) {
+      setError('Username is already taken')
+      return
+    }
+
+    if (user.name.length < 3) {
+      setError('Name must be atleast 3 characters long')
+      return
+    }
+
+    console.log('user', user)
+    console.log('username', username)
+
+    try {
+      const response = await axios.post('/api/user/updateUser', {
+        name: user.name,
+        username: username,
+        bio: user.bio,
+        about: user.about,
+      })
+      console.log('response', response.data.success)
+    } catch (error: any) {
+      console.log('error', error.response.data)
+    }
+  }
+
   return (
     <Screen className='flex'>
-      <div>Edit Profile </div>
+      <div className='grid gap-8'>
+        <div className='flex w-full text-center text-xl'>Edit Profile</div>
+        <div>
+          <div className='flex flex-col justify-center gap-2.5'>
+            <div>
+              <div className='px-1.5 text-lg font-medium text-black/60 dark:text-white/60'>Name</div>
+              <Input
+                type='text'
+                name='name'
+                placeholder='Enter your name'
+                leftIcon={<Ic Icon={User} />}
+                value={user.name}
+                onChange={(e: any) => {
+                  setUser({ ...user, name: e.target.value })
+                }}
+              />
+            </div>
+            <div>
+              <div className='px-1.5 text-lg font-medium text-black/60 dark:text-white/60'>UserName</div>
+
+              <Input
+                type='text'
+                name='userName'
+                placeholder='Choose a user name'
+                leftIcon={<Ic Icon={AtSign} />}
+                rightIcon={
+                  isUsernameChecking ? (
+                    <Ic Icon={LoaderCircle} className='animate-spin' />
+                  ) : isUsernameAvailable ? (
+                    <Ic Icon={Check} className='text-green-500' />
+                  ) : (
+                    username.length > 3 && <Ic Icon={X} className='text-red-500' />
+                  )
+                }
+                value={username}
+                onChange={(e: any) => {
+                  setUsername(e.target.value)
+                }}
+              />
+            </div>
+            <div>
+              <div className='px-1.5 text-lg font-medium text-black/60 dark:text-white/60'>Bio</div>
+              <textarea
+                className='h-36 w-full rounded-2xl border border-black/10 bg-black/5 px-4 py-4 text-sm font-medium text-black outline-none dark:border-white/10 dark:bg-white/5 dark:text-white'
+                name='bio'
+                placeholder='Enter your bio'
+                // leftIcon={<Ic Icon={User} />}
+                value={user.bio}
+                onChange={(e: any) => {
+                  setUser({ ...user, bio: e.target.value })
+                }}
+              />
+            </div>
+            <div>
+              <div className='px-1.5 text-lg font-medium text-black/60 dark:text-white/60'>About</div>
+              <textarea
+                className='h-36 w-full rounded-2xl border border-black/10 bg-black/5 px-4 py-4 text-sm font-medium text-black outline-none dark:border-white/10 dark:bg-white/5 dark:text-white'
+                name='about'
+                placeholder='Enter about yourself'
+                // leftIcon={<Ic Icon={User} />}
+                value={user.about}
+                onChange={(e: any) => {
+                  setUser({ ...user, about: e.target.value })
+                }}
+              />
+            </div>
+
+            {error && <Error error={error} />}
+          </div>
+        </div>
+        <Button title='Save' className='w-full' onClick={() => updateUser()}>
+          Save
+        </Button>
+      </div>
     </Screen>
   )
 }
---
+++ src/app/api/user/updateUser/route.ts
@@ -0,0 +1,72 @@
+import { connect } from '@/dbConfig/dbConfig'
+import { TokenDataT } from '@/lib/types'
+import User from '@/models/userModel'
+import { parseJson } from '@/utils/utils'
+import jwt from 'jsonwebtoken'
+import { NextRequest, NextResponse } from 'next/server'
+import { z } from 'zod'
+
+const userZ = z
+  .object({
+    name: z
+      .string({ required_error: 'Name is required' })
+      .trim()
+      .min(3, { message: 'Name must be at least 3 characters long' })
+      .max(100, { message: 'Name must be at most 100 characters long' })
+      .regex(/^[a-zA-Z\s]*$/, { message: 'Name must contain only letters' }),
+    username: z
+      .string({ required_error: 'Username is required' })
+      .trim()
+      .min(3, { message: 'Username must be at least 3 characters long' })
+      .max(50, { message: 'Username must be at most 50 characters long' })
+      .toLowerCase()
+      .regex(/^[a-zA-Z0-9]*$/, { message: 'Username must contain only letters and numbers' }),
+
+    bio: z
+      .string({ required_error: 'Bio is required' })
+      .trim()
+      .max(100, { message: 'Bio must be at most 100 characters long' })
+      .optional(),
+
+    about: z
+      .string({ required_error: 'About is required' })
+      .trim()
+      .max(1000, { message: 'About must be at most 1000 characters long' })
+      .optional(),
+  })
+  .strict()
+
+connect()
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  const body = await parseJson(req)
+  if (body instanceof NextResponse) return body
+  try {
+    const token = (await req.cookies.get('token')?.value) || ''
+    const tokenData = jwt.decode(token) as TokenDataT
+
+    if (!tokenData || !tokenData.isVerified) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 })
+    }
+
+    const { name, username, bio, about } = userZ.parse(body)
+
+    const user = await User.findOneAndUpdate({ _id: tokenData.id }, { name, username, bio, about }, { new: true })
+
+    if (!user) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 })
+    }
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: 'User updated successfully',
+      },
+      {
+        status: 200,
+      },
+    )
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 400 })
+  }
+}
