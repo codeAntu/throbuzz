cfaaa79d5c26c130078e707085523844844f9fe7
Ananta <codeantu@gmail.com>
2024-09-01 17:34:40 +0530

    refactor: Implement lazy loading for profile images and remove unused routes
    This commit implements lazy loading for profile images in the profile edit page. It also removes the unused routes for deleting, getting, uploading images and videos. These changes aim to improve page load performance and optimize the codebase by removing unnecessary code.

--- src/app/(client)/(user)/profile/[userName]/edit/page.tsx
+++ src/app/(client)/(user)/profile/[userName]/edit/page.tsx
@@ -3,7 +3,7 @@ import { Screen } from '@/components/Screen'
 export default function Edit() {
   return (
     <Screen className='flex'>
-      <div>Edit Profile "Anaanta "</div>
+      <div>Edit Profile </div>
     </Screen>
   )
 }
