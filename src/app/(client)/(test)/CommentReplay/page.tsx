ccfeb6797e7dbe29de4be3abe400a5bd2cc0040e
Ananta <codeantu@gmail.com>
2024-09-23 12:05:36 +0530

    refactor: Update CommentReplay component and related routes
    - Update CommentReplay component and related routes
    - Edit the content state in CommentReplay component
    - Add functionality to edit and delete comment replies in CommentReplay component
    - Implement API routes for editing and deleting comment replies

--- src/app/(client)/(test)/CommentReplay/page.tsx
+++ src/app/(client)/(test)/CommentReplay/page.tsx
@@ -5,15 +5,41 @@ import axios from 'axios'
 import { useState } from 'react'
 
 export default function CommentReplay() {
-  const [content, setContent] = useState('This is the nwe   replay  ')
+  const [content, setContent] = useState('This is the nwe   replay  , edited  ')
   const commentId = '66edb30ea16250bcfa06e392'
   const replyTo = 'codeAntu'
+  const commentReplyId = '66f05d5ff37ab0c3fcd63b37'
 
   async function replayComment() {
     try {
       const response = await axios.post('/api/activity/commentReply/createCommentReply', {
         content,
         commentId,
+      })
+
+      console.log(response.data)
+    } catch (error: any) {
+      console.error(error)
+    }
+  }
+
+  async function editCommentReply() {
+    try {
+      const response = await axios.post('/api/activity/commentReply/editCommentReplay', {
+        content,
+        commentReplyId,
+      })
+
+      console.log(response.data)
+    } catch (error: any) {
+      console.error(error)
+    }
+  }
+
+  async function deleteCommentReply() {
+    try {
+      const response = await axios.post('/api/activity/commentReply/deleteCommentReplay', {
+        commentReplyId,
       })
 
       console.log(response.data)
@@ -29,6 +55,12 @@ export default function CommentReplay() {
       <input type='text' />
       <Button onClick={replayComment} title='Replay'>
         Replay
+      </Button>
+      <Button onClick={editCommentReply} title='Edit'>
+        Edit
+      </Button>
+      <Button onClick={deleteCommentReply} title='Delete'>
+        Delete
       </Button>
     </Screen>
   )
---
+++ src/app/api/activity/commentReply/deleteCommentReplay/route.ts
@@ -0,0 +1,42 @@
+import { connect } from '@/dbConfig/dbConfig'
+import { TokenDataT } from '@/lib/types'
+import { NextRequest, NextResponse } from 'next/server'
+import { z } from 'zod'
+import jwt from 'jsonwebtoken'
+import CommentReply from '@/models/commentReplyModel'
+connect()
+
+const dataZ = z.object({
+  commentReplyId: z.string().nonempty(),
+})
+
+export async function POST(request: NextRequest) {
+  try {
+    const token = (await request.cookies.get('token')?.value) || ''
+    const tokenData = jwt.decode(token) as TokenDataT
+
+    if (!tokenData) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+    }
+
+    const userId = tokenData.id
+
+    const parseResult = dataZ.safeParse(await request.json())
+
+    if (!parseResult.success) {
+      return NextResponse.json({ error: 'Invalid input', details: parseResult.error.errors }, { status: 400 })
+    }
+
+    const { commentReplyId } = parseResult.data
+
+    const commentReply = await CommentReply.findByIdAndDelete(commentReplyId)
+
+    if (!commentReply) {
+      return NextResponse.json({ error: 'Comment reply not found' }, { status: 404 })
+    }
+
+    return NextResponse.json({ message: 'Comment reply deleted successfully', commentReply }, { status: 200 })
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 400 })
+  }
+}
